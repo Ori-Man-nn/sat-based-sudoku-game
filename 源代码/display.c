b389f3145f9a5d948a728aeaa282485a23080bf2
laoguapijiang <150890675+laoguapijiang@users.noreply.github.com>
2024-09-10 23:17:50 +0800

    Add files via upload

---
+++ 源代码/display.c
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdlib.h>
+#include <stdio.h>
+#include "cnfparser.h"
+#include "solver.h"
+#include "X-Sudoku.h"
+
+extern ArgueValue* ValueList;
+extern Root* r;
+
+
+
+int main(int argc, const char* argv[]) {
+    int op = 1;
+    while (op) {
+        system("cls");
+        printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+        printf("\t\t      #请选择功能#\n");
+        // printf("---------------------------------------------------------\n");
+        printf("\n");
+        printf("   1.Sudoku             2.SAT                0.exit\n");
+        printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+        scanf("%d", &op);
+        switch (op) {
+        case 1:
+            Sudoku();
+            break;
+        case 2:
+            SAT();
+            break;
+        case 0:
+            break;
+        default:
+            printf("输入错误！");
+            getchar(); getchar();
+            break;
+        }
+    }
+    return 0;
+}
---
+++ 源代码/solver.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#ifndef DPLL_h
+#define DPLL_h
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "cnfparser.h"
+
+
+
+status SAT(void);
+status DPLL1(int num, int op, int times);//采用第一种变元选取策略的递归算法DPLL函数
+status DPLL2(int num, int op, int times);//采用第二种变元选取策略的递归算法DPLL函数
+status DPLL3(int num, int op, int times);//采用第三种变元选取策略的递归算法DPLL函数
+status DPLL4(int num, int op, int times);//采用第四种变元选取策略的递归算法DPLL函数
+status DPLL(int num, int op, int times);//采用第一种变元选取策略的非递归算法DPLL函数
+status DPLL_Before(Root* r, int op);//优化前版本DPLL
+void AnswerComplete(void);//完善SAT求解结果
+status AnswerCheck(int solut);//检查SAT求解结果正确性
+
+#endif /* DPLL_h */
